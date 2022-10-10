c46fd1b274ffbbf12b5559566254e159557f1f5b
Shabbir Umrali <sumraliwala.contractor@libertyglobal.com>
2022-10-10 10:30:29 +0530

    changed port number and added mongo uri

--- app.js
+++ app.js
@@ -10,11 +10,17 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
+const dbURI = 'mongodb+srv://shabbir:1234@nodejwt.re9kr4c.mongodb.net/jsonwebtoken?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+  .then((result) => {
+    console.log("database connected");
+  })
   .catch((err) => console.log(err));
 
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+app.listen(8000, () => {
+  console.log("App listning at port 8000")
+})
