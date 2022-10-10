030984754c1d6bc8d5fcdf9cffe6ba81e280b7d7
Shabbir Umrali <sumraliwala.contractor@libertyglobal.com>
2022-10-10 15:41:18 +0530

    Added user model implement in authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,28 @@
+const User = require('../models/User');
+
+// Request      get     /signup
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
 
-module.exports.signup_post = (req, res) => {
+// Request      post     /signup
+module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
-    console.log(email, password);
-    res.send('new signup');
+
+    try {
+        const user = await User.create({ email, password });
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).send("User not created");
+    }
 }
 
+// Request      get     /login
 module.exports.login_get = (req, res) => {
     res.render('login');
 }
 
+// Request      post     /login
 module.exports.login_post = (req, res) => {
     console.log(req.body);
     res.send('user login');
