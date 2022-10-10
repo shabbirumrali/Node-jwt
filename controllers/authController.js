1b7c1bce5ad76ae2b21ceb5d051155a96bc05ee8
Shabbir Umrali <sumraliwala.contractor@libertyglobal.com>
2022-10-10 16:32:42 +0530

    error handlers for signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,15 +5,35 @@ module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
 
+// Handle Errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    const errors = {email: '', password: ''};
+
+    // duplicate error code
+    if(err.code === 11000) {
+        errors.email = "Email already exist enter new one or login with the same";
+        return errors;
+    }
+
+    // validation errors
+    if(err.message.includes('user validation failed')){
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message;
+        })
+    }
+    return errors
+}
+
 // Request      post     /signup
 module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
-
     try {
         const user = await User.create({ email, password });
         res.status(201).json(user);
-    } catch (error) {
-        res.status(400).send("User not created");
+    } catch (err) {
+        const error = handleErrors(err);
+        res.status(404).json({ error });
     }
 }
 
