96d2fdaff1614a61ec5e186ee348d1ceac82fe67
shabbirumrali <shabbirumrali@gmail.com>
2022-10-11 11:59:56 +0530

    cookie-parser, /set-cookie, /read-cookie

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -24,3 +26,18 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+// set cookie
+app.get('/set-cookie', (req, res) => {
+  // res.setHeader('Set-Cookie', "newUser=true");
+  res.cookie('user', true);
+  res.cookie('isEmployee', true);
+  res.send("you have got cookies");
+})
+
+// read cookie
+app.get('/read-cookie', (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+  res.json(cookies);
+})
