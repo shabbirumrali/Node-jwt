8e724139ea5cb7aed2eac2ddf14d77c848c78c05
Shabbir Umrali <sumraliwala.contractor@libertyglobal.com>
2022-10-11 12:10:31 +0530

    cookies work before the last update on cookie-parser

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -20,7 +22,18 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   })
   .catch((err) => console.log(err));
 
+// set cookies
+app.get('/set', (req, res) => {
+  // res.setHeader('Set-Cookie', 'newUser=true');
+  res.cookie("newUser", true);
+})
+
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+
+
+
+// read cookies
