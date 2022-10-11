667ded75ae2f0b53953dfd45cf02844c7955588a
Shabbir Umrali <sumraliwala.contractor@libertyglobal.com>
2022-10-12 00:35:41 +0530

    logout route and functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -74,3 +74,8 @@ module.exports.login_post = async (req, res) => {
         res.status(404).json({ error });
     }
 }
+
+module.exports.logout_get = (req, res) => {
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/');
+}
