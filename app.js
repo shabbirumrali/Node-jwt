ac24faf7d53dd7f716be01b50fb2aeb53d17d0d7
Shabbir Umrali <sumraliwala.contractor@libertyglobal.com>
2022-10-11 16:13:10 +0530

    Signup page with jwt & cookie setup

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
+
 
 // view engine
 app.set('view engine', 'ejs');
@@ -19,12 +22,6 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
     app.listen(8000);
   })
   .catch((err) => console.log(err));
-
-// set cookies
-app.get('/set', (req, res) => {
-  // res.setHeader('Set-Cookie', 'newUser=true');
-  res.cookie("newUser", true);
-})
 
 // routes
 app.get('/', (req, res) => res.render('home'));
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 // Request      get     /signup
 module.exports.signup_get = (req, res) => {
@@ -25,12 +26,19 @@ const handleErrors = (err) => {
     return errors
 }
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({ id }, 'Hello world key', { expiresIn: maxAge });
+}
+
 // Request      post     /signup
 module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.create({ email, password });
-        res.status(201).json(user);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(201).json({ user: user._id });
     } catch (err) {
         const error = handleErrors(err);
         res.status(404).json({ error });
