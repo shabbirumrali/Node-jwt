425a0d8ec0cc26ac75ea253160d9352a850f4bf2
Shabbir Umrali <sumraliwala.contractor@libertyglobal.com>
2022-10-12 10:57:21 +0530

    conditional renderning and outputing email using cookies

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
@@ -16,4 +17,26 @@ const requireAuth = (req, res, next) => {
     }
 }
 
-module.exports = { requireAuth }
+// check current user
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if(token) {
+        jwt.verify(token, 'Hello world key', async (err, decodedToken) => {
+            if(err) {
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            } else {
+                console.log(decodedToken);
+                const user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        })
+    } else {
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser }
